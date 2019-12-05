0a4bdb80c9a2f6522fece9c4942181e9c0808909
Papik Meli <papik.meli@gmail.com>
2019-12-05 23:13:07 +0100

    Implemented day 5.
    Implemented day 5 with tests.
    Extended computer.py with input and output, immediate mode, jumps, less than and equals instructions.

--- computer.py
+++ computer.py
@@ -5,17 +5,32 @@ from aoc import *
 pd = Debug(False)
 
 class Computer:
-    def __init__(self, data):
-        self.memory = data
-        self.ip = 0
+    def __init__(self, data, in_func=input, out_func=print):
         self.opcodes = {
                 1: self.add,
                 2: self.mul,
+                3: self.input,
+                4: self.output,
+                5: self.jump_if_true,
+                6: self.jump_if_false,
+                7: self.less_than,
+                8: self.equals,
                 99: self.halt
                 }
 
+        self.memory = data
+        self.in_func = in_func
+        self.out_func = out_func
+
+        self.ip = 0
+        self.output = []
+
     def step(self):
-        opcode = self.memory[self.ip]
+        instruction = self.get_addr(self.ip)
+        opcode= instruction % 100 # last 2 digits
+        pd('Opcode: ', opcode, 'at', self.ip)
+        self.modes = ((instruction // 100) % 10, (instruction // 1000) % 10, (instruction // 10000) % 10, )
+
         if opcode not in self.opcodes:
             pd("Error at {}, instruction {} unknown.".format(self.ip, opcode))
             raise Exception("Error at {}, instruction {} unknown.".format(self.ip, opcode))
@@ -33,20 +48,82 @@ class Computer:
         else:
             pd(r)
 
+    def get_param(self, param):
+        if self.modes[param - 1] == 0:
+            return self.get_ref(self.ip + param)
+        return self.get_addr(self.ip + param)
+
+    def set_param(self, param, val):
+        pd('Set', val, 'mode', self.modes[param - 1], 'at', self.ip + param)
+        if self.modes[param - 1] == 0:
+            return self.set_ref(self.ip + param, val)
+        return self.set_addr(self.ip + param, val)
+
+
     def add(self):
-        op1 = self.get_ref(self.ip + 1)
-        op2 = self.get_ref(self.ip + 2)
-        self.set_ref(self.ip + 3, op1 + op2)
+        op1 = self.get_param(1)
+        op2 = self.get_param(2)
+        self.set_param(3, op1 + op2)
         self.ip += 4
         return 'OK'
 
     def mul(self):
-        op1 = self.get_ref(self.ip + 1)
-        op2 = self.get_ref(self.ip + 2)
-        self.set_ref(self.ip + 3, op1 * op2)
+        op1 = self.get_param(1)
+        op2 = self.get_param(2)
+        self.set_param(3, op1 * op2)
         self.ip += 4
         return 'OK'
     
+    def input(self):
+        self.set_param(1, self.in_func())
+        self.ip += 2
+        return 'OK'
+
+    def output(self):
+        o = self.get_param(1)
+        self.output.append(o)
+        self.out_func(0)
+        self.ip += 2
+        return 'OK'
+
+    def jump_if_true(self):
+        op1 = self.get_param(1)
+        op2 = self.get_param(2)
+        if op1 != 0:
+            self.ip = op2
+            return 'OK'
+        self.ip += 3
+        return 'OK'
+
+    def jump_if_false(self):
+        op1 = self.get_param(1)
+        op2 = self.get_param(2)
+        if op1 == 0:
+            self.ip = op2
+            return 'OK'
+        self.ip += 3
+        return 'OK'
+
+    def less_than(self):
+        op1 = self.get_param(1)
+        op2 = self.get_param(2)
+        if op1 < op2:
+            self.set_param(3, 1)
+        else:
+            self.set_param(3, 0)
+        self.ip += 4
+        return 'OK'
+
+    def equals(self):
+        op1 = self.get_param(1)
+        op2 = self.get_param(2)
+        if op1 == op2:
+            self.set_param(3, 1)
+        else:
+            self.set_param(3, 0)
+        self.ip += 4
+        return 'OK'
+
     def halt(self):
         return 'HALT'
 
@@ -71,3 +148,5 @@ class Computer:
                 memory_str.append('{}'.format(self.memory[i]))
         print(' '.join(memory_str))
 
+    def get_output(self):
+        return self.output
