c1fd6183cf333e1c21b645ab60b2a214ac102e9e
remowxdx <papik.meli@gmail.com>
2019-12-18 00:29:36 +0100

    Modified day 16: element at offset is affected only by the elements after, and with a so high offset they are all "1".

--- aoc16.py
+++ aoc16.py
@@ -2,7 +2,7 @@
 
 from aoc import *
 
-pd = Debug(True)
+pd = Debug(False)
 DAY = 16
 
 def get_input(filename):
@@ -26,74 +26,58 @@ def fft_phase_1(data):
         r.append(abs(s) % 10)
     return r
 
-def fft_phase_2(data):
-    s = 0
-    rs = [s]
-    for i in data:
-        s += i
-        rs.append(s)
-    # print(rs)
-
+def fft_phase_2(data, offset):
     r = []
-    for n, i in enumerate(data):
-        if n > 2:
-            s = 0
-            rl = n + 1
-            i = n
-            while i < len(data):
-                s += rs[min(len(data), i+rl)] - rs[i]
-                i += 4 * rl
-            i = n + 2 * rl 
-            while i < len(data):
-                s += rs[i] - rs[min(len(data), i+rl)]
-                i += 4 * rl
-        else:
-            s = 0
-            rl = n + 1
-            i = n
-            while i < len(data):
-                s += sum(data[i:i+rl])
-                i += 4 * rl
-            i = n + 2 * rl 
-            while i < len(data):
-                s -= sum(data[i:i+rl])
-                i += 4 * rl
-        r.append(abs(s) % 10)
-    return r
+    s = 0
+    for i in range(len(data) - offset):
+        n = len(data) - i - 1
+        s += data[n]
+        r.append(s)
+    t = [abs(s) % 10 for s in r]
+    t.extend([0] * offset)
+    t.reverse()
+    return t
 
-def fft(data, phases):
+def fft_1(data, phases):
     r = data.copy()
     for i in range(phases):
-        print(f'Phase {i}...   ')
-        r = fft_phase_2(r)
-        print('\x1b[2A')
+        pd(f'Phase {i}...   ')
+        r = fft_phase_1(r)
+        pd('\x1b[2A')
     return r
 
+def fft_2(data, phases, offset):
+    r = data.copy()
+    for i in range(phases):
+        pd(f'Phase {i}...   ')
+        r = fft_phase_2(r, offset)
+        pd('\x1b[2A')
+    return r
 
 def test1(data, phases):
     a = [int(i) for i in data]
-    return ''.join([str(c) for c in fft(a, phases)[:8]])
+    return ''.join([str(c) for c in fft_1(a, phases)[:8]])
 
 def test2(data):
     a = [int(i) for i in data]
     real_data = a * 10_000
     offset = int(data[:7])
-    print(offset)
-    r = fft(real_data, 100)
+    pd(offset)
+    r = fft_2(real_data, 100, 0)
     # print(r)
     return ''.join([str(c) for c in r])[offset:offset+8]
 
 def part1(data, phases):
-    return ''.join([str(c) for c in fft(data, phases)[:8]])
+    return ''.join([str(c) for c in fft_1(data, phases)[:8]])
 
 def part2(data):
     real_data = data * 10_000
     offset = 0
     for i in data[:7]:
         offset = offset * 10 + i
-    print(offset)
-    r = fft(real_data, 100)
-    print(r)
+    pd(offset)
+    r = fft_2(real_data, 100, offset)
+    pd(r)
     return ''.join([str(c) for c in r])[offset:offset+8]
 
 if __name__ == '__main__':
