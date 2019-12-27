55801c47854ce88a892d0c274db9f6f23ed5c4c7
remowxdx <papik.meli@gmail.com>
2019-12-27 06:47:41 +0100

    Quasi solved day 18.

--- aoc18.py
+++ aoc18.py
@@ -7,254 +7,256 @@ DAY = 18
 
 def get_input(filename):
     with open(filename, 'r') as f:
-        lines = f.readlines()
-    return [l.strip() for l in lines]
+        return f.read()
+
+class Node:
+    def __init__(self, name, node_type):
+        self.name = name
+        self.type = node_type
+        self.links = []
+
+    def link_to(self, other, length=1):
+        if other not in self:
+            self.links.append((other, length))
+        if self not in other:
+            other.links.append((self, length))
+
+    def remove_link(self, other):
+        for i in range(len(self.links)):
+            node, dist = self.links[i]
+            if node == other:
+                self.links.pop(i)
+                break
+
+    def optimize(self):
+        t = self.type
+        if t.isupper() or t.islower() or t == '@':
+            return False
+
+        if len(self.links) == 1:
+            n1, d1 = self.links[0]
+            n1.remove_link(self)
+            self.links = []
+            return True
+
+        if len(self.links) == 2:
+            n1, d1 = self.links[0]
+            n2, d2 = self.links[1]
+
+            n1.remove_link(self)
+            n2.remove_link(self)
+            self.links = []
+
+            n1.link_to(n2, d1 + d2)
+            return True
+        return False
+            
+        
+
+    def __add__(self, other):
+        if other in self:
+            for node, dist in self.links:
+                if node == other:
+                    return dist
+            return None
+    
+    def __contains__(self, other):
+        for node, dist in self.links:
+                if node == other:
+                    return True
+        return False
+
+    def __str__(self):
+        l = ','.join([f'{n.name}-{d}' for n, d in self.links])
+        return f'{self.name} -> {l}'
 
 class Maze:
     def __init__(self, data):
-        self.maze = {}
-        self.pos = (0, 0)
-        self.keys = {}
-        self.steps = 0
-        self.visited = {}
-        self.parse(data)
+        self.portals = {}
+        self.nodes = {}
+        self.maze = data
+        self.starts = []
+        self.num_keys = 0
+        self.width = data.index('\n') + 1
+        self.height = len(data) // self.width
+        self.build_graph()
 
-    def get_pos(self, pos):
-        if pos in self.maze:
-            return self.maze[pos]
-        return '#'
+    def get(self, x, y):
+        if x < 0 or x >= self.width:
+            raise Exception(f'Out of x {(x, y)}')
+        if y < 0 or y >= self.height:
+            raise Exception(f'Out of y {(x, y)}')
+        return self.maze[y * self.width + x]
 
-    def set_pos(self, pos, typ):
-        self.maze[pos] = typ
+    def get_pos(self, x, y):
+        if (x,y) not in self.nodes:
+            return '#'
+        return self.nodes[(x,y)].type
 
-    def offset(self, pos, direction):
-        if direction == 0:
-            return (pos[0], pos[1]-1)
-        elif direction == 1:
-            return (pos[0]-1, pos[1])
-        elif direction == 2:
-            return (pos[0], pos[1]+1)
-        elif direction == 3:
-            return (pos[0]+1, pos[1])
-        else:
-            raise Exception('Unknown direction.')
+    def build_graph(self):
+        # Create nodes
+        for y in range(self.height):
+            for x in range(self.width):
+                t = self.get(x, y)
+                if t == '.' or t.isupper() or t.islower() or t == '@':
+                    self.nodes[(x,y)] = Node(f'({x}, {y})', t)
+                    if t.islower():
+                        self.num_keys += 1
+                    # pd('+', (x,y))
+                    if t == '@':
+                        self.starts.append(self.nodes[(x,y)])
+        # pd(self)
 
+        # Link nodes
+        for y in range(self.height):
+            for x in range(self.width):
+                # pd('Node:', (x,y))
+                t  = self.get_pos(x, y)
+                tl = self.get_pos(x-1, y)
+                tr = self.get_pos(x+1, y)
+                tu = self.get_pos(x, y-1)
+                td = self.get_pos(x, y+1)
 
-    def peek(self, direction):
-        return self.get_pos(self.offset(self.pos, direction))
-
-    def move(self, direction):
-        pos = self.offset(self.pos, direction)
-        if self.get_pos(pos) == '#':
-            raise Exception('Gone into wall.')
-        self.pos = pos
-
-    def parse(self, data):
-
-        if type(data) == str:
-            data = data.strip().split('\n')
-
-        self.limits = (len(data[0]), len(data))
-        for y, row in enumerate(data):
-            for x, cell in enumerate(row):
-                if cell == '#':
+                if t == '.' or t.isupper() or t.islower() or t == '@':
+                    if tr == '.' or tr.isupper() or tr.islower() or tr == '@':
+                        self.nodes[(x,y)].link_to(self.nodes[(x+1, y)], 1)
+                    if td == '.' or td.isupper() or td.islower() or td == '@':
+                        self.nodes[(x,y)].link_to(self.nodes[(x, y+1)], 1)
                     continue
-                if cell == '@':
-                    self.pos = (x, y)
-                    self.maze[(x,y)] = '.'
-                else:
-                    self.maze[(x,y)] = cell
 
-    def find_keys(self, pos, steps, visited, doors):
-        if pos in visited:
-            return {}
+    def optimize(self):
+        done = False
+        while not done:
+            done = True
+            for pos in self.nodes:
+                if pos in self.starts:
+                    continue
+                r = self.nodes[pos].optimize()
+                if r:
+                    done = False
 
-        new_visited = visited.copy()
-        new_visited[pos] = steps
-
-        keys_found = {}
-
-        cell = self.get_pos(pos)
-
-        # do we have to open a door?
-        if cell.isupper():
-            doors_found = doors.copy()
-            doors_found.append(cell)
-        else:
-            doors_found = doors
-
-        # did we find a key?
-        if cell.islower():
-            keys_found[cell] = (steps, doors_found)
-
-        for direction in range(4):
-            new_pos = self.offset(pos, direction)
-            m = self.get_pos(new_pos)
-
-            # it is a wall?
-            if m == '#':
-                continue
-
-            keys = self.find_keys(new_pos, steps + 1, new_visited, doors_found)
-
-            for k in keys:
-                keys_found[k] = keys[k]
-
-        # print(pos, steps, keys_found)
-        return keys_found
-
-    def find_path(self, pos, steps, with_keys):
-        possible_keys = self.find_keys(pos, steps, {}, with_keys)
-        # print(f'find_path: pos: {pos}, steps: {steps}, wk: {with_keys}, pos_keys: {possible_keys}')
-        if len(possible_keys) == 0:
-            return None
-
-
-        paths = {}
-
-        for k in possible_keys:
-            new_pos, next_steps = possible_keys[k]
-            new_keys = with_keys.copy()
-            new_keys.append(k)
-            new_paths = self.find_path(new_pos, next_steps, new_keys)
-            # print(f'NP: {new_paths}, key: {k}, steps: {steps+next_steps}, da pos: {pos}, steps: {steps}, wk: {with_keys}')
-            paths[k] = (next_steps, new_paths)
-        # print(f'           {paths}')
-        return paths
-
-    def find_all_keys(self):
-        self.keys = {}
-        for c in self.maze:
-            if self.maze[c].islower():
-                self.keys[self.maze[c]] = c
-
-    def find_distances(self):
-        dists = {}
-        all_keys = list(self.keys.keys())
-        for k in all_keys:
-            # print('Finding', k)
-            dists[k] = self.find_keys(self.keys[k], 0, {}, [])
-            # print(dists[k])
-        return dists
-
-    def calc_dist(self):
-        pass
+        to_remove = []
+        for x, y in self.nodes:
+            if len(self.nodes[(x,y)].links) == 0:
+                to_remove.append((x,y))
+        for p in to_remove:
+            self.nodes.pop(p)
 
     def __str__(self):
-        r = []
-        for y in range(self.limits[1]):
-            s = ''
-            for x in range(self.limits[0]):
-                if self.pos == (x,y):
-                    s += '@'
-                else:
-                    s += self.get_pos((x,y))
-            r.append(s)
-        return '\n'.join(r)
+        l = [f'W: {self.width}, L: {self.height}, NK: {self.num_keys}', ]
+        for p in self.nodes:
+            l.append(str(self.nodes[p]))
+        return '\n'.join(l)
 
-def min_path(paths):
-    mps = []
-    for k in paths:
-        # print(paths[k])
-        if paths[k][1] is None:
-            mps.append(paths[k][0])
-        else:
-            mps.append(min_path(paths[k][1]))
-    return min(mps)
+    def sp(self):
+        to_visit = [(self.starts[0], 0, set()), ]
+        visited = []
+        while len(to_visit) > 0:
+            m = to_visit[0]
+            for mc in to_visit:
+                if mc[1] < m[1]:
+                    m = mc
+
+            node, dist, keys = m
+            to_visit.remove(m)
+            pd(node.name, dist, keys)
+            visited.append((node, keys))
+            if len(keys) == self.num_keys:
+                return dist
+
+            for next_node, next_dist in node.links:
+                t = next_node.type
+                if t.isupper() and t.lower() not in keys:
+                    continue
+                next_keys = keys.copy()
+                if t.islower() and t not in keys:
+                    next_keys.add(t)
+                visit = True
+                for old_node, old_keys in visited:
+                    if next_node == old_node and next_keys == old_keys:
+                        visit = False
+                        break
+                for old_node, old_dist, old_keys in to_visit:
+                    if next_node == old_node and next_keys == old_keys:
+                        visit = False
+                        break
+                if visit:
+                    to_visit.append((next_node, dist + next_dist, next_keys))
+
+    def sp4(self):
+        to_visit = []
+        robots = []
+
+        for start in self.starts:
+            robots.append(start)
+
+        to_visit.append((robots, 0, set()))
+        visited = []
+        while len(to_visit) > 0:
+            m = to_visit[0]
+            for mc in to_visit:
+                if mc[1] < m[1]:
+                    m = mc
+
+            nodes, dist, keys = m
+            to_visit.remove(m)
+            pd(','.join([n.name for n in nodes]), dist, keys)
+            if dist > 100 and dist / len(keys) > 100:
+                continue
+            visited.append((nodes, keys))
+            if len(keys) == self.num_keys:
+                return dist
+
+            for r, node in enumerate(nodes):
+                for next_node, next_dist in node.links:
+                    t = next_node.type
+                    if t.isupper() and t.lower() not in keys:
+                        continue
+                    next_keys = keys.copy()
+                    next_nodes = nodes.copy()
+                    next_nodes[r] = next_node
+                    if t.islower() and t not in keys:
+                        next_keys.add(t)
+                    visit = True
+                    for old_nodes, old_keys in visited:
+                        if next_nodes == old_nodes and next_keys == old_keys:
+                            visit = False
+                            break
+                    for old_nodes, old_dist, old_keys in to_visit:
+                        if next_nodes == old_nodes and next_keys == old_keys:
+                            visit = False
+                            break
+                    if visit:
+                        to_visit.append((next_nodes, dist + next_dist, next_keys))
+        return "Ahhh!"
+
 
 def test1(data):
-    m = Maze(data)
-    p = m.find_path(m.pos, 0, [])
-    r = min_path(p)
-    return r
+    return 0
 
 def test2(data):
     return 0
 
-minimal = 10000
-
-def find_key_path(d, with_keys, steps):
-    global minimal
-    if steps >= minimal:
-        return None
-    if len(d)-1 == len(with_keys):
-        if steps < minimal:
-            minimal = steps
-            print(minimal)
-        print(with_keys, steps)
-        return with_keys, steps
-
-    paths = []
-    last_key = with_keys[-1]
-    key_possibilities = d[last_key]
-
-    good_keys = {}
-    for key in key_possibilities:
-        # if already taken do nothing
-        if key in with_keys:
-            continue
-
-        doors = key_possibilities[key][1]
-
-        # check if we have all needed keys
-        for door in doors:
-            if door.lower() not in with_keys:
-                break
-        else:
-            good_keys[key] = steps + key_possibilities[key][0]
-
-    
-    for key in sorted(good_keys, key=lambda it: good_keys[it]):
-        n_k = with_keys.copy()
-        n_k.append(key)
-        r = find_key_path(d, n_k, steps + key_possibilities[key][0])
-        if r is not None:
-            paths.append(r)
-
-    if len(paths) == 0:
-        return None
-
-    mini = paths[0]
-    for ks, steps in paths:
-        if steps < mini[1]:
-            mini = ks, steps
-    return mini
-            
-
-def find_path(d):
-    global minimal
-    minimal = 10000
-    paths = []
-    for key in d['@']:
-        print('Exploring: ', key, d['@'][key])
-        if len(d['@'][key][1]) == 0:
-            paths.append(find_key_path(d, [key, ], d['@'][key][0]))
-    mini = paths[0]
-    print(paths)
-    for kss in paths:
-        if kss is None:
-            continue
-        ks, steps = kss
-        if steps < mini[1]:
-            mini = ks, steps
-    return mini
-
 def part1(data):
     m = Maze(data)
-    m.find_all_keys()
-    print(m)
-    d = m.find_distances()
-    d['@'] = m.find_keys(m.pos, 0, {}, [])
-    print(d)
-    r = find_path(d)
-    print(r)
-    #print('Dists:', m.find_distances())
-    # p = m.find_path(m.pos, 0, [])
-    # r = min_path(p)
-    return r[1]
+    m.optimize()
+    pd(m)
+    return m.sp()
 
 def part2(data):
-    return None
+    nd = data.split('\n')
+    nd[39] = nd[39][:39] + '@#@' + nd[39][42:]
+    nd[40] = nd[40][:39] + '###' + nd[40][42:]
+    nd[41] = nd[41][:39] + '@#@' + nd[41][42:]
+
+    print('\n'.join(nd))
+    m = Maze('\n'.join(nd))
+    m.optimize()
+    pd(m)
+    print([n.name for n in m.starts])
+    r = m.sp4()
+    print(r)
+    return r
 
 if __name__ == '__main__':
 
@@ -265,12 +267,14 @@ if __name__ == '__main__':
 #f.D.E.e.C.b.A.@.a.B.c.#
 ######################.#
 #d.....................#
-########################'''
+########################
+'''
     test_input_3 = '''########################
 #...............b.C.D.f#
 #.######################
 #.....@.a.B.c.d.A.e.F.g#
-########################'''
+########################
+'''
     test_input_4 = '''#################
 #i.G..c...e..H.p#
 ########.########
@@ -279,33 +283,36 @@ if __name__ == '__main__':
 #k.E..a...g..B.n#
 ########.########
 #l.F..d...h..C.m#
-#################'''
+#################
+'''
     test_input_5 = '''########################
 #@..............ac.GI.b#
 ###d#e#f################
 ###A#B#C################
 ###g#h#i################
-########################'''
-    print('Test Part 1:')
-    test_eq('Test 1.1', part1, 8, test_input_1)
-    test_eq('Test 1.2', part1, 86, test_input_2)
-    test_eq('Test 1.3', part1, 132, test_input_3)
-    # test_eq('Test 1.4', part1, 136, test_input_4)
-    # test_eq('Test 1.5', part1, 81, test_input_5)
-    print()
+########################
+'''
+    if False:
+        print('Test Part 1:')
+        test_eq('Test 1.1', part1, 8, test_input_1)
+        test_eq('Test 1.2', part1, 86, test_input_2)
+        test_eq('Test 1.3', part1, 132, test_input_3)
+        test_eq('Test 1.4', part1, 136, test_input_4)
+        test_eq('Test 1.6', part1, 81, test_input_5)
+        print()
 
-    test_input_2 = [4,5,6]
-    print('Test Part 2:')
-    test_eq('Test 2.1', test2, 42, test_input_1)
-    print()
+        test_input_2 = [4,5,6]
+        print('Test Part 2:')
+        test_eq('Test 2.1', part2, 396, test_input_3)
+        print()
 
     data = get_input(f'input{DAY}')
 
-    r = part1(data)
-    # r = None
+    # r = part1(data)
+    r = None
     if r is not None:
         print('Part 1:', r)
-        save_solution(DAY, 1, r)
+        check_solution(DAY, 1, r)
 
     r = part2(data)
     if r is not None:
