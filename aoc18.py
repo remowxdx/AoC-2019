d6a0c8203bb46a4a3f35872f9336f78336283ef2
remowxdx <papik.meli@gmail.com>
2019-12-28 17:41:55 +0100

    Solved day 18, at last!!

--- aoc18.py
+++ aoc18.py
@@ -3,7 +3,7 @@
 from aoc import *
 import heapq
 
-pd = Debug(True)
+pd = Debug(False)
 DAY = 18
 
 def get_input(filename):
@@ -43,7 +43,7 @@ class Node:
         return self.name < other.name
 
     def shortcut(self):
-        if len(self.links) == 1:
+        if len(self.links) == 1 and self.type == '.':
             for l in self.links:
                 pass
             l.remove_link(self)
@@ -64,6 +64,9 @@ class Node:
         for m, n, d in to_add:
             if n not in m.links:
                 m.add_link(n, d)
+            else:
+                if d < m.links[n]:
+                    m.add_link(n, d)
                 # print(m.name, n.name, d)
 
     def __str__(self):
@@ -131,15 +134,15 @@ class Maze:
 
     def bfs(self):
         visited = [set() for _ in self.starts]
-        to_visit = [(0, 26, tuple(self.starts), ())]
+        to_visit = [(0, tuple(self.starts), ())]
         mkl = 0
         while len(to_visit) > 0:
-            l, lk, nodes, keys = heapq.heappop(to_visit)
+            l, nodes, keys = heapq.heappop(to_visit)
             
             if len(keys) == self.num_keys:
                 return l
             if len(keys) > mkl:
-                print(len(keys), len(to_visit))
+                pd(len(keys), len(to_visit))
                 mkl = len(keys)
             # print(len(to_visit), [len(n) for n in visited])
 
@@ -152,8 +155,6 @@ class Maze:
                 for new_node in n.links:
                     key = new_node.type
                     d = n.links[new_node]
-                    if (new_node, keys) in visited:
-                        continue
                     if key.isupper() and key.lower() not in keys:
                         continue
                     elif key.islower() and key not in keys:
@@ -170,8 +171,7 @@ class Maze:
                             new_nodes.append(new_node)
                         else:
                             new_nodes.append(nn)
-                    heapq.heappush(to_visit, (l + d, self.num_keys - len(new_keys), tuple(new_nodes), new_keys))
-
+                    heapq.heappush(to_visit, (l + d, tuple(new_nodes), new_keys))
 
     def __str__(self):
         maze = []
@@ -187,6 +187,7 @@ class Maze:
                     row.append('#')
             maze.append(''.join(row))
         return '\n'.join(maze)
+
 def test1(data):
     return 0
 
@@ -195,16 +196,11 @@ def test2(data):
 
 def part1(data):
     m = Maze(data)
-
     r = m.bfs()
     return r
 
 def part2(data):
     m = Maze(data)
-#    for p in m.nodes:
-#        if m.nodes[p].type.isupper():
-#            m.nodes[p].shortcut()
-
     r = m.bfs()
     return r
 
@@ -243,6 +239,14 @@ if __name__ == '__main__':
 ########################
 '''
     test_input_6 = '''#############
+#DcBa.#.GhKl#
+#.###@#@#I###
+#e#d#####j#k#
+###C#@#@###J#
+#fEbA.#.FgHi#
+#############
+'''
+    test_input_7 = '''#############
 #g#f.D#..h#l#
 #F###e#E###.#
 #dCba@#@BcIJ#
@@ -250,7 +254,8 @@ if __name__ == '__main__':
 #nK.L@#@G...#
 #M###N#H###.#
 #o#m..#i#jk.#
-#############'''
+#############
+'''
     if True:
         print('Test Part 1:')
         test_eq('Test 1.1', part1, 8, test_input_1)
@@ -260,10 +265,9 @@ if __name__ == '__main__':
         test_eq('Test 1.5', part1, 81, test_input_5)
         print()
 
-        test_input_2 = [4,5,6]
         print('Test Part 2:')
-        test_eq('Test 2.1', part2, 396, test_input_3)
-        test_eq('Test 2.2', part2, 72, test_input_6)
+        test_eq('Test 2.1', part2, 32, test_input_6)
+        test_eq('Test 2.2', part2, 72, test_input_7)
         print()
 
     data = get_input(f'input{DAY}')
