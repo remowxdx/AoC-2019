9b7a596a2f5f69c8f93563a2aa4b0449ff28aea9
remowxdx <papik.meli@gmail.com>
2019-12-28 17:21:57 +0100

    Solved day part 2.

--- aoc18.py
+++ aoc18.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from aoc import *
+import heapq
 
 pd = Debug(True)
 DAY = 18
@@ -11,226 +12,181 @@ def get_input(filename):
 
 class Node:
     def __init__(self, name, node_type):
-        self.name = name
+        self.links = {}
         self.type = node_type
-        self.links = []
-
-    def link_to(self, other, length=1):
-        if other not in self:
-            self.links.append((other, length))
-        if self not in other:
-            other.links.append((self, length))
-
-    def remove_link(self, other):
-        for i in range(len(self.links)):
-            node, dist = self.links[i]
-            if node == other:
-                self.links.pop(i)
-                break
-
-    def optimize(self):
-        t = self.type
-        if t.isupper() or t.islower() or t == '@':
-            return False
-
-        if len(self.links) == 1:
-            n1, d1 = self.links[0]
-            n1.remove_link(self)
-            self.links = []
-            return True
-
-        if len(self.links) == 2:
-            n1, d1 = self.links[0]
-            n2, d2 = self.links[1]
-
-            n1.remove_link(self)
-            n2.remove_link(self)
-            self.links = []
-
-            n1.link_to(n2, d1 + d2)
-            return True
-        return False
-            
-        
+        self.name = name
 
     def __add__(self, other):
-        if other in self:
-            for node, dist in self.links:
-                if node == other:
-                    return dist
-            return None
-    
+        return self.add_link(other, 1)
+
+    def add_link(self, other, dist):
+        self.links[other] = dist
+        other.links[self] = dist
+
+    def remove_link(self, other):
+        if other in self.links:
+            self.links.pop(other)
+        # else:
+            # print(self.name, other.name)
+        if self in other.links:
+            other.links.pop(self)
+        # else:
+            # print(other.name, self.name)
+
+    def __sub__(self, other):
+        self.remove_link(other)
+
     def __contains__(self, other):
-        for node, dist in self.links:
-                if node == other:
-                    return True
-        return False
+        return other in self.links
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def shortcut(self):
+        if len(self.links) == 1:
+            for l in self.links:
+                pass
+            l.remove_link(self)
+            return
+
+        to_remove = []
+        to_add = []
+        for n in self.links:
+            for m in self.links:
+                if n == m:
+                    continue
+                to_add.append((m, n, self.links[n] + self.links[m]))
+            to_remove.append(n)
+
+        for r in to_remove:
+            self.remove_link(r)
+
+        for m, n, d in to_add:
+            if n not in m.links:
+                m.add_link(n, d)
+                # print(m.name, n.name, d)
 
     def __str__(self):
-        l = ','.join([f'{n.name}-{d}' for n, d in self.links])
-        return f'{self.name} -> {l}'
+        s0 = f'{self.name:8} - {self.type} -> '
+        s = []
+        for link in self.links:
+            s.append(f'{link.name:8} - {self.links[link]}')
+        return s0 + ', '.join(s)
 
 class Maze:
     def __init__(self, data):
-        self.portals = {}
         self.nodes = {}
-        self.maze = data
-        self.starts = []
+        self.cut_nodes = {}
         self.num_keys = 0
-        self.width = data.index('\n') + 1
-        self.height = len(data) // self.width
-        self.build_graph()
+        self.width = 0
+        self.height = 0
+        self.starts = []
+        self.build_graph(data)
 
-    def get(self, x, y):
-        if x < 0 or x >= self.width:
-            raise Exception(f'Out of x {(x, y)}')
-        if y < 0 or y >= self.height:
-            raise Exception(f'Out of y {(x, y)}')
-        return self.maze[y * self.width + x]
-
-    def get_pos(self, x, y):
-        if (x,y) not in self.nodes:
-            return '#'
-        return self.nodes[(x,y)].type
-
-    def build_graph(self):
+    def build_graph(self, data):
         # Create nodes
-        for y in range(self.height):
-            for x in range(self.width):
-                t = self.get(x, y)
-                if t == '.' or t.isupper() or t.islower() or t == '@':
-                    self.nodes[(x,y)] = Node(f'({x}, {y})', t)
-                    if t.islower():
-                        self.num_keys += 1
-                    # pd('+', (x,y))
-                    if t == '@':
-                        self.starts.append(self.nodes[(x,y)])
-        # pd(self)
+        x = 0
+        y = 0
+        nodes = {}
+        for c in data:
+            if c == '#':
+                x += 1
+                continue
+            elif c == '\n':
+                y += 1
+                x = 0
+            elif c == '.' or c.islower() or c.isupper() or c == '@':
+                nodes[(x,y)] = Node(str((x,y)), c)
+                if c == '@':
+                    self.starts.append(nodes[(x,y)])
+                if c.islower():
+                    self.num_keys += 1
+                x += 1
+            else:
+                raise 'Unknown node.'
+
+        self.width = x - 1
+        self.height = y
 
         # Link nodes
-        for y in range(self.height):
-            for x in range(self.width):
-                # pd('Node:', (x,y))
-                t  = self.get_pos(x, y)
-                tl = self.get_pos(x-1, y)
-                tr = self.get_pos(x+1, y)
-                tu = self.get_pos(x, y-1)
-                td = self.get_pos(x, y+1)
+        for x, y in nodes:
+            if (x - 1, y) in nodes:
+                nodes[(x, y)] + nodes[(x - 1, y)]
+            if (x, y - 1) in nodes:
+                nodes[(x, y)] + nodes[(x, y - 1)]
 
-                if t == '.' or t.isupper() or t.islower() or t == '@':
-                    if tr == '.' or tr.isupper() or tr.islower() or tr == '@':
-                        self.nodes[(x,y)].link_to(self.nodes[(x+1, y)], 1)
-                    if td == '.' or td.isupper() or td.islower() or td == '@':
-                        self.nodes[(x,y)].link_to(self.nodes[(x, y+1)], 1)
+        # Optimize graph
+        for n in nodes:
+            if nodes[n].type == '.':
+                nodes[n].shortcut()
+
+        for n in nodes:
+            if len(nodes[n].links) > 0:
+                self.nodes[n] = nodes[n]
+                # print(nodes[n])
+            else:
+                self.cut_nodes[n] = nodes[n]
+
+        # print(self)
+
+    def bfs(self):
+        visited = [set() for _ in self.starts]
+        to_visit = [(0, 26, tuple(self.starts), ())]
+        mkl = 0
+        while len(to_visit) > 0:
+            l, lk, nodes, keys = heapq.heappop(to_visit)
+            
+            if len(keys) == self.num_keys:
+                return l
+            if len(keys) > mkl:
+                print(len(keys), len(to_visit))
+                mkl = len(keys)
+            # print(len(to_visit), [len(n) for n in visited])
+
+            # print([node.name for node in nodes], l, keys)
+
+            for i, n in enumerate(nodes):
+                if (n, keys) in visited[i]:
                     continue
+                visited[i].add((n, keys))
+                for new_node in n.links:
+                    key = new_node.type
+                    d = n.links[new_node]
+                    if (new_node, keys) in visited:
+                        continue
+                    if key.isupper() and key.lower() not in keys:
+                        continue
+                    elif key.islower() and key not in keys:
+                        kl = list(keys)
+                        kl.append(key)
+                        kl.sort()
+                        new_keys = tuple(kl)
+                    else:
+                        new_keys = keys
 
-    def optimize(self):
-        done = False
-        while not done:
-            done = True
-            for pos in self.nodes:
-                if pos in self.starts:
-                    continue
-                r = self.nodes[pos].optimize()
-                if r:
-                    done = False
+                    new_nodes = []
+                    for j, nn in enumerate(nodes):
+                        if i == j:
+                            new_nodes.append(new_node)
+                        else:
+                            new_nodes.append(nn)
+                    heapq.heappush(to_visit, (l + d, self.num_keys - len(new_keys), tuple(new_nodes), new_keys))
 
-        to_remove = []
-        for x, y in self.nodes:
-            if len(self.nodes[(x,y)].links) == 0:
-                to_remove.append((x,y))
-        for p in to_remove:
-            self.nodes.pop(p)
 
     def __str__(self):
-        l = [f'W: {self.width}, L: {self.height}, NK: {self.num_keys}', ]
-        for p in self.nodes:
-            l.append(str(self.nodes[p]))
-        return '\n'.join(l)
-
-    def sp(self):
-        to_visit = [(self.starts[0], 0, set()), ]
-        visited = []
-        while len(to_visit) > 0:
-            m = to_visit[0]
-            for mc in to_visit:
-                if mc[1] < m[1]:
-                    m = mc
-
-            node, dist, keys = m
-            to_visit.remove(m)
-            pd(node.name, dist, keys)
-            visited.append((node, keys))
-            if len(keys) == self.num_keys:
-                return dist
-
-            for next_node, next_dist in node.links:
-                t = next_node.type
-                if t.isupper() and t.lower() not in keys:
-                    continue
-                next_keys = keys.copy()
-                if t.islower() and t not in keys:
-                    next_keys.add(t)
-                visit = True
-                for old_node, old_keys in visited:
-                    if next_node == old_node and next_keys == old_keys:
-                        visit = False
-                        break
-                for old_node, old_dist, old_keys in to_visit:
-                    if next_node == old_node and next_keys == old_keys:
-                        visit = False
-                        break
-                if visit:
-                    to_visit.append((next_node, dist + next_dist, next_keys))
-
-    def sp4(self):
-        to_visit = []
-        robots = []
-
-        for start in self.starts:
-            robots.append(start)
-
-        to_visit.append((robots, 0, set()))
-        visited = []
-        while len(to_visit) > 0:
-            m = to_visit[0]
-            for mc in to_visit:
-                if mc[1] < m[1]:
-                    m = mc
-
-            nodes, dist, keys = m
-            to_visit.remove(m)
-            pd(','.join([n.name for n in nodes]), dist, keys)
-            if dist > 100 and dist / len(keys) > 100:
-                continue
-            visited.append((nodes, keys))
-            if len(keys) == self.num_keys:
-                return dist
-
-            for r, node in enumerate(nodes):
-                for next_node, next_dist in node.links:
-                    t = next_node.type
-                    if t.isupper() and t.lower() not in keys:
-                        continue
-                    next_keys = keys.copy()
-                    next_nodes = nodes.copy()
-                    next_nodes[r] = next_node
-                    if t.islower() and t not in keys:
-                        next_keys.add(t)
-                    visit = True
-                    for old_nodes, old_keys in visited:
-                        if next_nodes == old_nodes and next_keys == old_keys:
-                            visit = False
-                            break
-                    for old_nodes, old_dist, old_keys in to_visit:
-                        if next_nodes == old_nodes and next_keys == old_keys:
-                            visit = False
-                            break
-                    if visit:
-                        to_visit.append((next_nodes, dist + next_dist, next_keys))
-        return "Ahhh!"
-
-
+        maze = []
+        for y in range(self.height+1):
+            row = []
+            for x in range(self.width+1):
+                if (x,y) in self.nodes:
+                    row.append(self.nodes[(x,y)].type)
+                elif (x,y) in self.cut_nodes:
+                    # row.append(self.cut_nodes[(x,y)].type)
+                    row.append(' ')
+                else:
+                    row.append('#')
+            maze.append(''.join(row))
+        return '\n'.join(maze)
 def test1(data):
     return 0
 
@@ -239,23 +195,17 @@ def test2(data):
 
 def part1(data):
     m = Maze(data)
-    m.optimize()
-    pd(m)
-    return m.sp()
+
+    r = m.bfs()
+    return r
 
 def part2(data):
-    nd = data.split('\n')
-    nd[39] = nd[39][:39] + '@#@' + nd[39][42:]
-    nd[40] = nd[40][:39] + '###' + nd[40][42:]
-    nd[41] = nd[41][:39] + '@#@' + nd[41][42:]
+    m = Maze(data)
+#    for p in m.nodes:
+#        if m.nodes[p].type.isupper():
+#            m.nodes[p].shortcut()
 
-    print('\n'.join(nd))
-    m = Maze('\n'.join(nd))
-    m.optimize()
-    pd(m)
-    print([n.name for n in m.starts])
-    r = m.sp4()
-    print(r)
+    r = m.bfs()
     return r
 
 if __name__ == '__main__':
@@ -292,29 +242,40 @@ if __name__ == '__main__':
 ###g#h#i################
 ########################
 '''
-    if False:
+    test_input_6 = '''#############
+#g#f.D#..h#l#
+#F###e#E###.#
+#dCba@#@BcIJ#
+#############
+#nK.L@#@G...#
+#M###N#H###.#
+#o#m..#i#jk.#
+#############'''
+    if True:
         print('Test Part 1:')
         test_eq('Test 1.1', part1, 8, test_input_1)
         test_eq('Test 1.2', part1, 86, test_input_2)
         test_eq('Test 1.3', part1, 132, test_input_3)
         test_eq('Test 1.4', part1, 136, test_input_4)
-        test_eq('Test 1.6', part1, 81, test_input_5)
+        test_eq('Test 1.5', part1, 81, test_input_5)
         print()
 
         test_input_2 = [4,5,6]
         print('Test Part 2:')
         test_eq('Test 2.1', part2, 396, test_input_3)
+        test_eq('Test 2.2', part2, 72, test_input_6)
         print()
 
     data = get_input(f'input{DAY}')
 
-    # r = part1(data)
-    r = None
+    r = part1(data)
     if r is not None:
         print('Part 1:', r)
         check_solution(DAY, 1, r)
 
+    data = get_input(f'input{DAY}-2')
+
     r = part2(data)
     if r is not None:
         print('Part 2:', r)
-        save_solution(DAY, 2, r)
+        check_solution(DAY, 2, r)
